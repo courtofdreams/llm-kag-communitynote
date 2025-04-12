ee1f19a04b03c3b95daed1491fb25b7bfe24f8f1
Supakarn B <30888519+courtofdreams@users.noreply.github.com>
2025-04-12 16:26:13 -0700

    Added accuracy, F1 score, precision, and recall calculations for system evaluation.

--- Application.py
+++ Application.py
@@ -1,7 +1,41 @@
 from KnowledgeAgent import KnowledgeAgent
-from KnowledgeGraphService import KnowledgeGraphService
+from KnowledgeGraphService import KnowledgeGraphService, Graph
+import json
+import os
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
  
 kg_service = KnowledgeGraphService(0.1,"gpt-4o-2024-08-06")
 agent = KnowledgeAgent(kg_service=kg_service)
 
-agent.answer_query("Did Elon Musk buy Meta?", config="hybrid")
+## For testing purposes, we will use the politifact dataset
+gold_answers = []
+questions = []
+for file in os.listdir("data"):
+    if file.endswith(".json"):
+        with open(os.path.join("data", file), "r") as f:
+            data = json.load(f)  
+            if isinstance(data, dict):
+                questions.append(data["question"])
+                gold_answers.append(data["is_factual"])
+            else:
+                raise ValueError("Expected a JSON object, but got something else.")
+
+system_answers = []
+for question in tqdm(questions):
+    system_answer = agent.answer_query(question, Graph.COMMUNITY)
+    system_answers.append(system_answer["is_factual"])
+    print(f"System Answer: {system_answer["answer"]}")
+    
+# calculate accuracy of 
+accuracy = accuracy_score(gold_answers, system_answers)
+f1 = f1_score(gold_answers, system_answers, average='weighted')
+precision = precision_score(gold_answers, system_answers, average='weighted')
+recall = recall_score(gold_answers, system_answers, average='weighted')
+print(f"Accuracy: {accuracy}")
+print(f"F1 Score: {f1}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+
+
+# TODO: Add configurations for community and politifact based on the user input
--- KnowledgeGraphService.py
+++ KnowledgeGraphService.py
@@ -6,32 +6,58 @@ from langchain_core.documents import Document
 from langchain_neo4j import Neo4jGraph, GraphCypherQAChain
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
+from enum import Enum
 
+class Graph(Enum):
+    COMMUNITY = "community"
+    POLITIFACT = "politifact"
+    BASELINE = "baseline"
+    
 class KnowledgeGraphService:
         def __init__(self, temperature=0, model_name="gpt-4-turbo"):   
             OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
             if OPENAI_API_KEY is None:
                 raise ValueError("OPENAI_API_KEY environment variable not set.")
-            self.graphDB = Neo4jGraph()
-            print(f"{OPENAI_API_KEY}, {temperature}, {model_name}")
+
+            self.graph_politifact = Neo4jGraph(
+                database=Graph.POLITIFACT.value
+            )
+
+            self.graph_community = Neo4jGraph(
+                database=Graph.COMMUNITY.value
+            )
+            
             self.llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY,temperature=temperature, model_name=model_name)
             self.llm_transformer = LLMGraphTransformer(
                 llm=self.llm
             )          
         
-        def build_graph(self, text):
+        def build_graph(self, text, config: Graph):
             documents = [Document(page_content=text)]
             graph_documents = self.llm_transformer.convert_to_graph_documents(documents)
             print(f"Nodes:{graph_documents[0].nodes}")
             print(f"Relationships:{graph_documents[0].relationships}")
-            self.graphDB.add_graph_documents(graph_documents) # store into database       
+            if config == Graph.COMMUNITY:
+                self.graph_community.add_graph_documents(graph_documents)
+            elif config == Graph.POLITIFACT:
+                self.graph_politifact.add_graph_documents(graph_documents)
+            else:
+                raise ValueError("Invalid config. Choose 'community' or 'politifact'.")   
             
-        def reset_database(self):
-            with self.graphDB.session() as session:
-                session.run("MATCH (n) DETACH DELETE n")
-                print("Database reset successfully!")   
+        def reset_database(self, config: Graph):
+            if config == Graph.COMMUNITY:
+                with self.graph_community.session() as session:
+                    session.run("MATCH (n) DETACH DELETE n")
+                    print("Database reset successfully!")   
         
-        def get_facts(self, query):
+            elif config == Graph.POLITIFACT:
+                with self.graph_politifact.session() as session:
+                    session.run("MATCH (n) DETACH DELETE n")
+                    print("Database reset successfully!")   
+            else:
+                raise ValueError("Invalid config. Choose 'community' or 'politifact'.")
+
+        def get_facts(self, query, config: Graph):
             custom_prompt_template = """
                 check the factuality of the statement and give out the result as yes or no
                 Statement: {question}
@@ -40,15 +66,38 @@ class KnowledgeGraphService:
             CUSTOM_PROMPT = PromptTemplate(
                 template=custom_prompt_template, input_variables=["question"]
             )
-            chain = GraphCypherQAChain.from_llm(
-                llm=self.llm,
-                verbose=True,
-                allow_dangerous_requests=True,
-                graph=self.graphDB,
-                qa_prompt=CUSTOM_PROMPT,
-            )
-            result = chain.run({'query':query}) # result = yes
-            return result
+            if config == Graph.COMMUNITY:
+                chain = GraphCypherQAChain.from_llm(
+                    llm=self.llm,
+                    verbose=True,
+                    allow_dangerous_requests=True,
+                    graph=self.graph_community,
+                    qa_prompt=CUSTOM_PROMPT,
+                )
+                result = chain.run({'query':query}) # result = yes
+                return result
+            elif config == Graph.POLITIFACT:
+                print(f"Querying {config.value} database with query: {query}")
+                chain = GraphCypherQAChain.from_llm(
+                    llm=self.llm,
+                    verbose=True,
+                    allow_dangerous_requests=True,
+                    graph=self.graph_politifact,
+                    qa_prompt=CUSTOM_PROMPT,
+                )
+                result = chain.run({'query':query})
+                return result
+            elif config == Graph.BASELINE: # TODO baseline = only use the LLM, Need to decide what model to use., this will call openai or xxx directly
+                chain = GraphCypherQAChain.from_llm(
+                    llm=self.llm,
+                    verbose=True,
+                    allow_dangerous_requests=True,
+                    graph=None,
+                    qa_prompt=CUSTOM_PROMPT,
+                )
+                return result
+            else:    
+                raise ValueError("Invalid config. Choose 'community' or 'politifact'.")    
                         
         def close(self):
             self.graphDB.close()
